00da8af100c333adde27b8dd0362467dcd4c6cc0
Mycheny <1208640961@qq.com>
2020-05-28 18:20:22 +0800

    更新结构

--- code/model.py
+++ code/model.py
@@ -3,16 +3,45 @@
 # @Author wcy
 import numpy as np
 import tensorflow as tf
-
+import keras
+import keras.backend as K
+import keras.layers as KL
+import keras.engine as KE
+import keras.models as KM
 from triplet import Triplet
+
+
+class BatchNorm(KL.BatchNormalization):
+    """Extends the Keras BatchNormalization class to allow a central place
+    to make changes if needed.
+
+    Batch normalization has a negative effect on training if batches are small
+    so this layer is often frozen (via setting in Config class) and functions
+    as linear layer.
+    """
+    def call(self, inputs, training=None):
+        """
+        Note about training values:
+            None: Train BN layers. This is the normal mode
+            False: Freeze BN layers. Good when batch size is small
+            True: (don't use). Set layer in training mode even when making inferences
+        """
+        return super(self.__class__, self).call(inputs, training=training)
 
 
 class Model(Triplet):
 
     def __init__(self):
         super().__init__()
+        self.input_query_vector = tf.placeholder(tf.float32, (None, 768))  # shape (batch, 768)
+
+        self.input_feature_vector = tf.placeholder(tf.float32, (None, None, 2048))  # shape (batch, boxe_num, 2048)
+        self.input_boxe_vector = tf.placeholder(tf.float32, (None, None, 3))  # shape (batch, boxe_num, (x, y, area_than)
+        self.input_label_vector = tf.placeholder(tf.float32, (None, None, 768))  # shape (batch, boxe_num, 768)
+        self.build()
 
     def build(self):
+        x = KL.Conv2D(128, (3, 3), padding="valid")(tf.zeros((32, 28, 28, 3), dtype=tf.float32))
         dim = 128
         batch = 5
         labels = np.random.randint(0, 5, (batch,))
@@ -23,4 +52,4 @@ class Model(Triplet):
 
 
 if __name__ == '__main__':
-    pass
+    Model()
