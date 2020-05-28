5bf70276eaebb34613b03136b0d6420f16307a9f
Mycheny <1208640961@qq.com>
2020-05-28 10:29:42 +0800

    规范项目结构

--- code/dataset.py
+++ code/dataset.py
@@ -1,3 +1,14 @@
 # -*- coding: utf-8 -*- 
 # @Time 2020/5/27 14:16
 # @Author wcy
+import tensorflow as tf
+import numpy as np
+
+
+class Dataset(object):
+
+    def __init__(self):
+        pass
+
+    def next(self):
+        pass
--- code/main.py
+++ code/main.py
@@ -1,3 +1,9 @@
 # -*- coding: utf-8 -*- 
 # @Time 2020/5/27 14:17
 # @Author wcy
+import tensorflow as tf
+import numpy as np
+
+
+if __name__ == '__main__':
+    pass
